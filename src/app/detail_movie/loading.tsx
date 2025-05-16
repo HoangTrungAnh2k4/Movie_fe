451b5863a048fbf95562159cfd9ade5c1f128e89
HoangTrungAnh <anh.hoang04k22@hcmut.edu.vn>
2025-05-16 09:16:42 +0700

    fix animation loading

--- src/app/detail_movie/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-    return (
-        <div className="z-50 fixed inset-0 flex justify-center items-center bg-background">
-            <div className="border-4 border-primary border-t-transparent rounded-full w-16 h-16 animate-spin" />
-        </div>
-    );
-}
